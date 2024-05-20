4061836c59f8b451af420a117e39082c9d5de255
Moon-12 <ashwijanayak@gmail.com>
2024-05-20 14:06:42 -0400

    signup page initial version

--- src/components/SignUpPage/SignUpPage.jsx
+++ src/components/SignUpPage/SignUpPage.jsx
@@ -1,5 +1,51 @@
+import { useState } from "react";
+import uiData from "../env/common_ui_metadata.json";
+import "./SignUpPage.css";
+
 const SignUpPage = () => {
-  return <div>Sign-up page</div>;
+  const [inputFields, setInputFields] = useState({
+    name: "",
+    userName: "",
+    password: "",
+    confirmPass: "",
+  });
+  const [isFormValid, setIsFormValid] = useState(false);
+  const { signUp } = uiData;
+
+  const handleInputChange = (event) => {
+    let data = { ...inputFields };
+    console.log(data);
+    data[event.target.name] = event.target.value;
+    setInputFields(data);
+  };
+
+  const submit = (event) => {
+    event.preventDefault();
+    console.log(inputFields);
+  };
+
+  return (
+    <div className="signup-container">
+      <h2>{signUp.heading}</h2>
+      <form onSubmit={submit}>
+        {signUp.data.map((field) => {
+          return (
+            <input
+              type={field.type}
+              placeholder={field.placeholder}
+              name={field.name}
+              value={inputFields[field.name]}
+              onChange={handleInputChange}
+            />
+          );
+        })}
+
+        <button onClick={submit} value="Sign Up" disabled={!isFormValid}>
+          Sign Up
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default SignUpPage;
