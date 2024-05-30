e48037a0d0bf06b434b21e54eb02b12dac61241a
Moon-12 <ashwijanayak@gmail.com>
2024-05-29 22:33:22 -0400

    removed unused import

--- src/components/routes/PrivateRoutes.jsx
+++ src/components/routes/PrivateRoutes.jsx
@@ -2,7 +2,6 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
-import { clearToken } from "../../redux/slice/authSlice";
 
 const PrivateRoute = () => {
   const loginResponse = useSelector((state) => state.auth.loginResponse);
